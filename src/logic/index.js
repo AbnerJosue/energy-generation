07ea197d7fd48c1fa4c2614237825db115c8939b
AbnerJosue <josueestrada688@gmail.com>
2023-05-13 16:46:48 -0600

    changes services

--- src/logic/index.js
+++ src/logic/index.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api  = createApi({
     reducerPath: 'splitApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://backend-services-vttu.onrender.com/' }),
     endpoints: () => ({}),
 })
